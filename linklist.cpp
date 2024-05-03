312ca683be410b373b9726686aa6de221d43ed52
LindaPermata <permatalinda531@gmail.com>
2024-05-03 14:02:58 +0700

    memasukkan node baru ditengah atau di akhir node

--- linklist.cpp
+++ linklist.cpp
@@ -35,6 +35,18 @@ void addNode()
         newNode->prev = NULL;
         START = newNode;
     }
+    else
+    {
+        Node *current = START;
+        Node *previous = NULL;
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+    }
 
 }
 
