92ebaeb213f0cd8ff24ba928e69d8ba2834a8679
LindaPermata <permatalinda531@gmail.com>
2024-05-03 15:01:46 +0700

    membuat switch casen3

--- linklist.cpp
+++ linklist.cpp
@@ -210,6 +210,9 @@ int main()
             case '2':
                 deleteNode();
                 break;
+            case '3':
+                traverse();
+                break
         }
         
     
