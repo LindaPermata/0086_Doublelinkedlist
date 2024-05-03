ea301c7b50a040f33f1279e95513dd9c3013a534
LindaPermata <permatalinda531@gmail.com>
2024-05-03 13:47:42 +0700

    membuat new node step 1

--- linklist.cpp
+++ linklist.cpp
@@ -13,7 +13,14 @@ Node *START = NULL;
 
 void addNode()
 {
+    Node *newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
     
+
 }
 
 
